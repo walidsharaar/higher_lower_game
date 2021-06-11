0030b7d1a609b981f8d826604436c9f0248d8a4f
Sharaar <walidsharar@gmail.com>
2021-06-11 14:40:40 +0200

    loop the game till getting wrong answer

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def get_different_account():
 # todo format the account data
 def format_data(account):
     """formating the dictionary data into printable format."""
-    account_name = account["name"]
-    account_descrp= account["description"]
-    account_country= account["country"]
-    return(f"{account_name}, a {account_descrp} , from {account_country}")
+    name = account["name"]
+    description= account["description"]
+    country= account["country"]
+    return(f"{name}, a {description} , from {country}")
 
 
 def check_answer(guess,first_account_follower,second_account_follower):
@@ -26,35 +26,39 @@ def check_answer(guess,first_account_follower,second_account_follower):
         return guess =="b"
 
 
+def play_game():
+    score= 0
+    game_continue = True
+    #todo display art
+    print(logo)
+    # todo generate random from the list of data
+    first_account= random.choice(data)
+    second_account= random.choice(data)
+#todo loop the game until getting wrong answer
+    while game_continue:
+        first_account = second_account
+        second_account = get_different_account()
+#todo loop to generate different account
+    while first_account == second_account:
+    # ensure two accounts are different
+        second_account=random.choice(data)
 
-#todo display art
-print(logo)
+    print(f"Compare A : {format_data(first_account)}")
+    print(vs)
+    print(f"Compare B : {format_data(second_account)}")
 
-# todo generate random from the list of data
+    #todo ask user guess
 
-first_account= random.choice(data)
-second_account= random.choice(data)
-# ensure two accounts are different
-if first_account == second_account:
-    second_account=random.choice(data)
+    guess = input("Who has the more followers? A or B  ").lower()
+    first_account_follower = first_account["follower_count"]
+    second_account_follower = second_account["follower_count"]
 
-print(f"Compare A : {format_data(first_account)}")
-print(vs)
-print(f"Compare B : {format_data(second_account)}")
-
-#todo ask user guess
-
-guess = input("Who has the more followers? A or B  ").lower()
-first_account_follower = first_account["follower_count"]
-second_account_follower = second_account["follower_count"]
-
-# todo check the user guess
-is_right = check_answer(guess,first_account_follower,second_account_follower)
-score = 0
-if is_right:
-    score +=1
-    print(f"You guess right! your score is {score}")
-else:
-    print(f"Sorry you are wrong. Your score is : {score}")
+    # todo check the user guess
+    is_right = check_answer(guess,first_account_follower,second_account_follower)
+    if is_right:
+        score +=1
+        print(f"You guess right! your score is {score}")
+    else:
+        print(f"Sorry you are wrong. Your score is : {score}")
 
 
