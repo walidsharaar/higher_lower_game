a9540ef23807826212f07bdeedd974a6f8004b3c
Sharaar <walidsharar@gmail.com>
2021-06-11 14:05:11 +0200

    format account data

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ print(logo)
 
 first_account= random.choice(data)
 second_account= random.choice(data)
-print(first_account)
-print(second_account)
+# ensure two accounts are different
+if first_account == second_account:
+    second_account=random.choice(data)
 
+# todo format the account data
+
+account_name =first_account["name"]
+account_descrp= first_account["description"]
+account_country= first_account["country"]
+
+print(f"{account_name}, a {account_descrp} , from {account_country}")
 
 
 
